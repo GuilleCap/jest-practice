8949a425805ed6681bfa5e61b488d44a62381d3a
Guille <Guille_d2@live.com>
2021-12-12 14:28:52 -0500

    I cant ensure the list of milanesa

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,6 +4,15 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
+   
+   const myCookbook = new Cookbook();
+  const myCookbookCli = new CookbookCli(myCookbook);
+
+  const message = myCookbookCli.add('burguer', ['meat', 'bread']);
+
+  myCookbookCli.add('burguer', ['meat', 'bread']);
+
+  expect(message).toEqual('Successfully added the following recipe: burguer');
 
     });
   });
@@ -11,11 +20,34 @@ describe('CookbookCli', () => {
   describe('Listing recipes', () => {
     test('should display the correct message listing all of the recipe names', () => {
 
+    const myCookbook = new Cookbook();
+    const myCookbookCli = new CookbookCli(myCookbook);
+    
+    const message = myCookbookCli.list('milanesa', ['meat', 'bread crumbs', 'eggs']);
+    
+
+   myCookbookCli.list('milanesa', ['meat', 'bread crumbs', 'eggs']);
+   
+
+   
+
+   expect(message).toEqual('You have the following recipes: milanesa');
+
+
     });
   });
 
   describe('Retrieving a recipe', () => {
-    test('should display the ingredients required to make the specified recipe', () => {
+                const myCookbook = new Cookbook();
+	        const myCookbookCli = new CookbookCli(myCookbook);
+
+	        myCookbookCli.add('biriyani',['rice','chicken','spices']);
+	        myCookbookCli.add('hotdog',['bun','sausage','ketchup']);
+
+	        const message =  myCookbookCli.get('hotdog');
+
+	        expect(message).toEqual('The ingredients for hotdog are: bun, sausage and ketchup');
+	  test('should display the ingredients required to make the specified recipe', () => {
 
     });
   });
